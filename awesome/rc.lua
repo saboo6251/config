ae7d5e0a5c9fef6a1af1f74acf48fc0f83701a6c
saboo6251 <saboo6250@gmail.com>
2025-10-29 10:01:03 +0000

    Change chosen theme from second to first

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -48,7 +48,7 @@ end
 local themes = { 
  	         "ghost-black" --1
 	       }
-local chosen_theme = themes [2]
+local chosen_theme = themes [1]
 --beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua") -- default directory
 beautiful.init(string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme))
 -- This is used later as the default terminal and editor to run.
